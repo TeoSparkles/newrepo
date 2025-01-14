eb3f97595bf9e8a62e57ed62c759281d9295adee
TeoSparkles <catapusantp@byui.edu>
2025-01-14 14:26:10 +0800

    Committing

--- server.js
+++ server.js
@@ -11,18 +11,19 @@ const app = express()
 const static = require("./routes/static")
 const expressLayouts = require("express-ejs-layouts")
 
-/* ***********************
- * View Engine and Templates
- *************************/
-app.set("view engine", "ejs")
-app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at views root
 
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
 
 
 //Index route for the index.
