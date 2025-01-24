43a794d08134ec54b84135a41b3de516ac421055
TeoSparkles <catapusantp@byui.edu>
2025-01-24 10:06:28 +0800

    Updating the SQL Assignment

---
+++ database/assignment2.sql
@@ -0,0 +1,30 @@
+SELECT * FROM inventory;
+
+
+--Insert the account of the record
+
+-- INSERT INTO account (account_firstname, account_lastname, account_email, account_password) VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
+-- UPDATE account SET account_type = 'Admin' WHERE account_firstname='Tony' account_lastname='Stark';
+-- DELETE FROM account WHERE account_firstname='Tony';
+-- SELECT * FROM account;
+
+-- UPDATE SET WHERE 
+
+-- Modify the "GM Hummer" record to read "a huge interior" rather than "small interiors" using a single query.
+UPDATE inventory 
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior') WHERE inv_make = 'GM' AND inv_model = Hummer;
+
+-- Use an inner join to select the make and model fields from the inventory table and the classification name field 
+-- from the classification table for inventory items that belong to the "Sport" category.
+-- Advice: Take note of the variable of the inclusions with the table followed by the column type.
+SELECT i.inv_make, i.inv_model, c.classification_name FROM INVENTORY i 
+INNER JOIN classification c ON i.classification_id = c.classification_id WHERE classification_name = 'Sport'
+
+
+-- Update all records in the inventory table to add "/vehicles" to the middle of the file path in the inv_image and inv_thumbnail columns using a single query.
+UPDATE inventory SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'), 
+inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
+
+
+-- Read from the inventory table.
+SELECT * FROM INVENTORY;
